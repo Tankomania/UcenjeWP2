ce9716b7dddc8e6b9db3abf8df35078a07090058
Antonio Kelava <kelava.antonio392@gmail.com>
2024-02-10 09:46:34 +0100

    Gitignore fix, Ucenje update

---
+++ CSHARP/UcenjeWP2/UcenjeCS/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Linq.Expressions;
+using UcenjeCS;
+
+//E01UlazIzlaz.Izvedi();
+
+//E02VarijableTipoviPodatakaOperatori.Izvedi();
+
+//E03UvjetnaGrananja.Izvedi();
+
+//E04Nizovi.Izvedi();
+
+//E05ForPetlja.Izvedi();
+
+//E06WhilePetlja.Izvedi();
+
+//E07DoWhilePetlja.Izvedi();
+
+//E08ForEachPetlja.Izvedi();
+
+//E09Metode.Izvedi();
+
+//Console.WriteLine(Z01Metode.CijeliDioZbroja(3.4,5.8));
+
+//E10ObradaIznimki.Izvedi();
+
+//E11Rekurzija.Izvedi();
+
+//Z02Vjezba.Izvedi();
+
+//UcenjeCS.E12KlasaObjekt.Program.Izvedi();
+
+//UcenjeCS.E13Nasljedivanje.Program.Izvedi();
+
+//UcenjeCS.E14Polimorfizam.Program.Izvedi();
+
+// ČITATI: https://github.com/tjakopec/OOP_JAVA_PHP_PYTHON_SWIFT
+
+
+//new UcenjeCS.E15KonzolnaAplikacija.Izbornik();
+
+//new UcenjeCS.E16Lambda.Program();
+
+//new UcenjeCS.E17Delegati.Program();
+
+new UcenjeCS.E18Ekstenzije.Program();
